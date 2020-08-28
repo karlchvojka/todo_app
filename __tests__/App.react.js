fa5e39a224301d69a516699ffd7db9b52df5c8e7
Karl Chvojka <karl.chvojka@protonmail.com>
2020-08-28 18:31:52 -0400

    Wrote beginning setup and tests for the Task list

--- __tests__/App.react.js
+++ __tests__/App.react.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import {render} from '@testing-library/react'
+import {render} from '@testing-library/react';
+import { create, renderer } from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
 
 import App from '../src/App.js';
+import TaskList from '../src/components/Organisms/TaskList';
 
-it('should render properly', () => {
-  const { asFragment } = render(<App />)
-  expect (asFragment(<App />)).toMatchSnapshot()
+
+describe("The App component", () => {
+  it('should render properly', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<App />);
+    const result = renderer.getRenderOutput();
+    expect(result.type).toBe('div');
+    expect(result.props.children).toEqual([
+      <header>
+        <h1>Todo App</h1>
+      </header>,
+      <main id="mainWrap">
+        <TaskList />
+      </main>
+    ]);
+  })
 });
---
+++ src/components/Organisms/TaskList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+
+const TaskList = (props) => {
+  return (
+    <section className="taskList">
+    </section>
+  )
+}
+
+export default TaskList
