05dc23f0befd92587c168a829484048c0dee1965
Karl Chvojka <karl.chvojka@protonmail.com>
2020-08-30 19:26:25 -0400

    Some final changes

--- __tests__/App.react.js
+++ __tests__/App.react.js
@@ -3,10 +3,8 @@ import {render} from '@testing-library/react';
 import { create, renderer } from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
 
-
 import App from '../src/App.js';
 import TaskList from '../src/components/Organisms/TaskList';
-
 
 describe("The App component", () => {
   it('should render properly', () => {
