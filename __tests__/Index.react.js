31d878c82bf11061190c30e83ba89a28bc455a32
Karl Chvojka <karl.chvojka@protonmail.com>
2020-08-30 20:00:41 -0400

    configured jest a bit more

---
+++ __tests__/Index.react.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/App.js';
+
+describe("The App mounts properly", () => {
+  it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+});
