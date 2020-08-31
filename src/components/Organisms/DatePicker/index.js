d63cbf0796109593ebd5fe87aa007bc979dbcf6e
Karl Chvojka <karl.chvojka@protonmail.com>
2020-08-30 20:46:32 -0400

    Started taskform

---
+++ src/components/Organisms/DatePicker/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+
+const Datepicker = (props) => {
+  return (
+    <section className="datepicker">
+    </section>
+  )
+}
+
+export default Datepicker
