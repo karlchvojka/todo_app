51000af3d5df5ae687868cea156beb8b971f4c16
Karl Chvojka <karl.chvojka@protonmail.com>
2020-08-31 21:09:54 -0400

    Days are now pulled in dynamically, and showing 3 months worth of days from Momentjs rather than hardcoded

--- src/components/Organisms/DatePicker/index.js
+++ src/components/Organisms/DatePicker/index.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
 import Moment from 'react-moment';
+import moment from 'moment';
+import { extendMoment } from 'moment-range';
+const moment2 = extendMoment(moment);
 
 function Datepicker(props) {
-  const now = new Date();
-  console.log("now :", now)
-  const month = new Date(now.getFullYear(), now.getMonth()+1, 0).getDate();
-  console.log('month: ', month);
-  const items = [];
+  const [days, setDays] = useState([])
 
-  for (let day = 0; day < month; day++ ) {
-    items.push(
-      <div className="day">
-        <p>{day + 1}</p>
-        </div>
-      )
-  }
+  useEffect(() => {
+    const currMonth = new Date().getMonth() + 1;
+    const currYear = new Date().getYear() + 1900;
+    const start = new Date(currYear, (currMonth - 1 ), 0);
+    const end   = new Date(currYear, (currMonth + 1 ), 0);
+    const range = moment2.range(start, end);
+    let daysArr = [];
 
+    for (let day of range.by('day')) {
+      daysArr.push(
+        day['_d']
+        .toString()
+      );
+    }
+    setDays(daysArr)
+  }, [])
 
+  const dayList = days.map((day, index) => {
+    return (
+      <div key={index} className="day"><p>{day.split(' ')[2]}</p></div>
+    )
+  })
+
+  console.log('daylist: ', dayList)
   return (
     <section className="datepicker">
-      {items}
+    {dayList}
     </section>
   )
 }
--- src/components/Organisms/TaskForm/index.js
+++ src/components/Organisms/TaskForm/index.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
 
 import Datepicker from '../DatePicker'
 
+function onDatePick(e) {
+  e.preventDefault();
+  // DO STUFF
+  // 1. Show date DatePicker
+
+  // 2. return back the date selected
+}
+
 function TaskForm() {
+  const [picker, setPicker] = useState({});
+
+
   return (
     <section className="taskForm">
       <form action="/newtweet" method="POST">
-        <input type="text" name="title" />
-        <textarea id="textAreaMain" name="text"></textarea>
+        <div className="titleWrap">
+          <label>Task Title:<br/>
+            <input type="text" name="taskTitle" />
+          </label>
+        </div>
+
+        <div className="dateWrap">
+          <div className="startDate">
+            <label>Start Date:<br/>
+              <input type="text" name="startDate" onClick={onDatePick}/>
+            </label>
+          </div>
+
+          <div className="endDate">
+            <label>End Date:<br/>
+              <input type="text" name="endDate" />
+            </label>
+          </div>
+        </div>
+
+        <div className="descWrap">
+          <label>Description:<br/>
+            <textarea id="textAreaMain" name="taskText"></textarea>
+          </label>
+        </div>
+
         <input type="submit" value="tweet" />
-      </form>
-      <Datepicker />
-    </section>
+        </form>
+      <Datepicker chooseDate={setPicker} />
+      </section>
   )
 }
 
